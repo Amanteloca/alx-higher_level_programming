788babc5061635493b88c9d3b6e033843a8fdac9
amanteloca <tonyanaky51@gmail.com>
2023-04-04 00:34:26 -0700

    update

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,103 +1,26 @@
 #!/usr/bin/python3
+Rectangle = __import__('4-rectangle').Rectangle
 
-""" more practice with classes!!! """
+my_rectangle = Rectangle(2, 4)
+print(str(my_rectangle))
+print("--")
+print(my_rectangle)
+print("--")
+print(repr(my_rectangle))
+print("--")
+print(hex(id(my_rectangle)))
+print("--")
 
-class Rectangle:
+# create new instance based on representation
+new_rectangle = eval(repr(my_rectangle))
+print(str(new_rectangle))
+print("--")
+print(new_rectangle)
+print("--")
+print(repr(new_rectangle))
+print("--")
+print(hex(id(new_rectangle)))
+print("--")
 
-    """ initialize with the width and height with value checks
-
-    Args:
-
-        width: how fat will this 4polygon be
-
-        height: how tall this box is
-
-    Return: nonezo
-
-    """
-
-    def __init__(self, width=0, height=0):
-
-        self.width = width
-
-        self.height = height
-
-    @property
-
-    def width(self):
-
-        return self.__width
-
-    @width.setter
-
-    def width(self, value):
-
-        if type(value) is not int:
-
-            raise TypeError("width must be an integer")
-
-        if value < 0:
-
-            raise ValueError("width must be >= 0")
-
-        self.__width = value
-
-    @property
-
-    def height(self):
-
-        return self.__height
-
-    @height.setter
-
-    def height(self, value):
-
-        if type(value) is not int:
-
-            raise TypeError("height must be an integer")
-
-        if value < 0:
-
-            raise ValueError("height must be >= 0")
-
-        self.__height = value
-
-    def area(self):
-
-        """ calculates the area of this rekt """
-
-        return self.__width * self.__height
-
-    def perimeter(self):
-
-        """ calculates the perimeter of this rect """
-
-        if self.__width is 0 or self.__height is 0:
-
-            return 0
-
-        return (self.__width) * 2 + (self.__height * 2)
-
-    def __str__(self):
-
-        string = ""
-
-        if self.__width is 0 or self.__height is 0:
-
-            return string
-
-        for i in range(self.__height):
-
-            for j in range(self.__width):
-
-                string += "#"
-
-            if i is not self.__height - 1:
-
-                string += "\n"
-
-        return string
-
-    def __repr__(self):
-
-        return ("Rectangle({}, {})".format(self.__width, self.__height))
+print(new_rectangle is my_rectangle)
+print(type(new_rectangle) is type(my_rectangle))
